67d511060c9354dd8db7a9d3ddbf217dcb9ed94a
sinwonkim <zhfps114@gmail.com>
2019-05-01 20:02:18 +0900

    # ==,equals() what is difference?

--- WebMaster/src/ReferenceType.java
+++ WebMaster/src/ReferenceType.java
@@ -9,5 +9,20 @@ public class ReferenceType {
 		}else {
 			System.out.println("참조가 다름");
 		}
+		
+		String strVar3 = new String("김테스트");
+		String strVar4 = new String("김테스트");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("참조가 같음");
+		}else {
+			System.out.println("참조가 다름");
+		}
+		
+		if(strVar3.equals(strVar4)) {
+			System.out.println("문자열이 같음");
+		}else {
+			System.out.println("문자열이 다름");
+		}
 	}
 }
